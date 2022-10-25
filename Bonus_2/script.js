5fd230cc195aa6b1d6f7c0fab30e5ad4b609300c
clmb00 <paolo.colombo2000@gmail.com>
2022-10-25 09:49:44 +0200

    [FIX] event

--- Bonus_2/script.js
+++ Bonus_2/script.js
@@ -23,39 +23,35 @@ autoplay();
 
 const thumbElements = document.getElementsByClassName('thumb');
 
-document.getElementById("up").addEventListener('click', changeImageUp);
+document.getElementById("up").addEventListener('click', function(){
+  changeImage(false);
+});
 
-document.getElementById("down").addEventListener('click', changeImageDown);
+document.getElementById("down").addEventListener('click', function(){
+  changeImage(true);
+});
 
 const stopAutoplay = () => clearInterval(cicle);
 
-document.querySelector('.container').addEventListener('mouseover', stopAutoplay);
+document.querySelector('.container').addEventListener('mouseenter', stopAutoplay);
 
-document.querySelector('.container').addEventListener('mouseout', autoplay);
+document.querySelector('.container').addEventListener('mouseleave', autoplay);
 
 function autoplay(){
   cicle = setInterval(function(){
-    changeImageDown();
+    changeImage(true);
   }, timeAutoplay);
 }
 
-function changeImageDown(){
+function changeImage(down){
   thumbElements[counter].classList.replace('active', 'black_op');
-  counter ++;
-  if (counter>imgList.length - 1){
-    counter = 0;
-  };
+  if (down) {
+    counter ++;
+    if (counter>imgList.length - 1) counter = 0;
+  } else {
+    counter --;
+    if (counter<0) counter = imgList.length - 1;
+  }
   thumbElements[counter].classList.replace('black_op', 'active');
   document.querySelector('#main_img img').src = "../img/" + imgList[counter];
 }
-
-function changeImageUp(){
-  thumbElements[counter].classList.replace('active', 'black_op');
-  counter --;
-  if (counter<0){
-    counter = imgList.length - 1;
-  };
-  thumbElements[counter].classList.replace('black_op', 'active');
-  document.querySelector('#main_img img').src = "../img/" + imgList[counter];
-}
-
